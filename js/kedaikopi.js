3ba04d909495a0ac72f4be35f7b54bbaa8112e7a
kelvin <muhammadfalahalfarisi@gmail.com>
2023-10-11 10:49:06 +0800

    add structure main, img coffe n js navbar

---
+++ js/kedaikopi.js
@@ -0,0 +1,19 @@
+// Toggle Class Active
+const navbarNav = document.querySelector('.navbar-nav');
+
+// Ketika Hamburger Menu Di Klik
+document.querySelector('#hamburger-menu').onclick = () => {
+    navbarNav.classList.toggle('active');
+}
+
+// Klik Di Luar Sidebar  Untuk Menghilankan Nav
+const hamburger = document.querySelector('#hamburger-menu');
+
+document.addEventListener('click', function(e) {
+    if (!hamburger.contains(e.target) && !navbarNav.contains(e.target)) {
+        navbarNav.claassList.remove('active');
+    }
+});
+
+
+

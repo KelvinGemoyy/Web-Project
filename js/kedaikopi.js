cba47e98cc931679313992f4d36a8eeaf5c1ec54
kelvin <muhammadfalahalfarisi@gmail.com>
2023-10-12 08:09:12 +0800

    add img,img flip n bg img coffe

--- js/kedaikopi.js
+++ js/kedaikopi.js
@@ -2,14 +2,14 @@
 const navbarNav = document.querySelector('.navbar-nav');
 
 // Ketika Hamburger Menu Di Klik
-document.querySelector('#hamburger-menu').onclick = () => {
+document.querySelector('#hamburger-menu').onClick = () => {
     navbarNav.classList.toggle('active');
 }
 
 // Klik Di Luar Sidebar  Untuk Menghilankan Nav
 const hamburger = document.querySelector('#hamburger-menu');
 
-document.addEventListener('click', function(e) {
+document.addEventListener('click', function (e) {
     if (!hamburger.contains(e.target) && !navbarNav.contains(e.target)) {
         navbarNav.claassList.remove('active');
     }

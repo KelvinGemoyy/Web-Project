fa9f5c08145dd4b65efbce720a968cee0c4122c7
kelvin <muhammadfalahalfarisi@gmail.com>
2023-10-18 07:58:52 +0800

    add search event in javascript

--- js/kedaikopi.js
+++ js/kedaikopi.js
@@ -1,10 +1,20 @@
-// Toggle Class Active
+// Toggle Class Active (Hamburger Menu)
 const navbarNav = document.querySelector('.navbar-nav');
 
 // Ketika Hamburger Menu Di Klik
 document.querySelector('#hamburger-menu').onClick = () => {
     navbarNav.classList.toggle('active');
 }
+
+// Toggle Class Active (Search Form)
+const searchForm = document.querySelector('.search-form');
+const searchBox = document.querySelector('#search-box');
+
+document.querySelector('#search-button').onClick = (e) => {
+    searchForm.classList.toggle('active');
+    searchBox.focus();
+    e.preventDefault();
+};
 
 // Klik Di Luar Sidebar  Untuk Menghilankan Nav
 const hamburger = document.querySelector('#hamburger-menu');
